ad1db8e3ca00c52cf2f6917dde6d81ddccc09bbb
shuangxiangkan <18550887212@163.com>
2020-11-01 20:28:40 +0800

    delete unrelevant files

--- identify/test4.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-main="darknet.exe detector test data/coco.data yolov3.cfg yolov3.weights -i 0 -thresh 0.25 -dont_show people.jpg"
-# os.popen(main)
-# os.system(main)
-content=os.popen(main).read()
-print(content)
-# time.sleep(10)
